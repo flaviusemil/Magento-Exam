d7a56d2c3304795993beb4d93e798da845ef8b33
Titus Toia <titus.toia@gmail.com>
2019-04-10 23:55:45 +0300

    2. added controller

---
+++ app/code/local/Magentoclass/Banners/controllers/IndexController.php
@@ -0,0 +1,10 @@
+<?php
+class Magentoclass_Banners_IndexController extends Mage_Core_Controller_Front_Action
+{
+    public function indexAction()
+    {
+        echo "<h2>Welcome to MagentoClass</h2>";
+
+        //echo "<h4>Number of featured products:</h4>";
+    }
+}
